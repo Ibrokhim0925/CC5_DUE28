36ccab91fd7a2bee57cb1c59445d33bb7a722549
Ibrokhim Ushurov <ibrokhimushurov@usf.edu>
2025-09-28 21:58:53 -0400

    Writing and testing a calculateBasePay function

--- cc_5.js
+++ cc_5.js
@@ -6,3 +6,15 @@ const employees = [
     { name: "Diana", hourlyRate: 45, hoursWorked: 40 }
 ];
 
+
+// Step 3: Write a function of calculateBasePay
+function calculateBasePay (hourlyRate, hoursWorked) {
+    const regularHours = Math.min(hoursWorked, 40);
+    return hourlyRate * regularHours;
+}
+
+console.log("Testing calculateBasePay");
+console.log("Test 1: (under 40 hours):", calculateBasePay(22, 30));
+console.log("Test 2: (over 40 hours):", calculateBasePay(25, 50));
+console.log("\n");
+
